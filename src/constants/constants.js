f1bd1d717b0bf0b65d4a53d12c53118d28964865
Stanley Hayford <hayfordstanley@gmail.com>
2022-03-31 20:00:22 +0000

    minor error

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,2 @@
-const SERVER_BASE_URL = 'https://birthcert.herokuapp.com//api/v1/user';
-
+const SERVER_BASE_URL = 'https://birthcert.herokuapp.com/api/v1/user';
 export const CONSTANTS = {SERVER_BASE_URL};
